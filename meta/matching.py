d2e587204f0350b16241b51e89fa5015a63b8b83
MichaelChuai <alexanderm16@163.com>
2019-11-01 10:58:23 +0800

    m

--- meta/matching.py
+++ meta/matching.py
@@ -1,57 +1,35 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 
 
-
-class Embedding(nn.Module):
-    def __init__(self, in_channels, out_channels, dropout_prob=0.1):
-        super(Embedding, self).__init__()
-        self.layer1 = self.convLayer(in_channels, out_channels, dropout_prob)
-        self.layer2 = self.convLayer(out_channels, out_channels, dropout_prob)
-        self.layer3 = self.convLayer(out_channels, out_channels, dropout_prob)
-        self.layer4 = self.convLayer(out_channels, out_channels, dropout_prob)
+class MatchingNetwork(nn.Module):
+    def __init__(self, embedding_network, context_embedding_network, num_classes):
+        super(MatchingNetwork, self).__init__()
+        self.embedding_network = embedding_network
+        self.context_embedding_network = context_embedding_network
+        self.num_classes = num_classes
+        if self.num_classes == -1:
+            self.classification = False
+        else:
+            self.classification = True
+        self.similarities = None
 
     @classmethod
-    def convLayer(cls, in_channels, out_channels, dropout_prob):
-        seq = nn.Sequential(nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
-        nn.BatchNorm2d(out_channels),
-        nn.ReLU(),
-        nn.MaxPool2d(kernel_size=2, stride=2),
-        nn.Dropout(dropout_prob)
-        )
-        return seq
+    def build_context_embedding_network(cls, in_dim, out_dim, num_layers, bidirectional=True):
+        network = nn.LSTM(in_dim, hidden_size=out_dim, num_layers=num_layers, bidirectional=bidirectional)
+        return network
 
-    def forward(self, inputs):
-        x = self.layer1(inputs)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
-        x = x.view(x.size(0), -1)
-        return x
+    def attention(self, inputs, support_x, support_y):
+        similarities = torch.softmax(torch.cosine_similarity(support_x, inputs.unsqueeze(dim=1), dim=-1), dim=-1)
+        if self.classification:
+            support_y_pre = F.one_hot(support_y, self.num_classes)
+        else:
+            support_y_pre = support_y.unsqueeze(dim=-1)
+        output = torch.bmm(similarities.unsqueeze(dim-1), support_y_pre.type(torch.float32)).squeeze(dim=1)
+        return similarities, output
 
-
-
-
-
-
-lstm = nn.LSTM(10, hidden_size=30, num_layers=3, bidirectional=True)
-G = Embedding(5, 10)
-
-# 10-way
-# 2-shot
-
-ax = torch.rand(32, 20, 5, 28, 28, dtype=torch.float32) # support_x
-ay = torch.randint(10, (32, 20))
-
-# enclst = []
-# for i in range(a.size(1)):
-#     enclst.append(G(a[:,i,:,:]).unsqueeze(dim=1))
-# x1 = torch.cat(enclst, dim=1)
-# x2, (hn, cn) = lstm(x1)
-
-
-# support_set = torch.rand(32, 10, 64, dtype=torch.float32)
-
-# input_image = torch.rand(32, 64)
-# sim = torch.cosine_similarity(support_set, input_image.unsqueeze(dim=1), dim=-1)
+    def forward(self, inputs, support_x, support_y):
+        
+        
