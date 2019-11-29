71ba73d67eae3004c97f935483c536de9494fafa
MichaelChuai <alexanderm16@163.com>
2019-11-29 13:44:36 +0800

    m

--- matching_test.py
+++ matching_test.py
@@ -3,7 +3,6 @@ import torch
 from meta.matching import *
 import dlutil as dl
 
-
 shots = 2
 ways = 5
 
@@ -13,7 +12,7 @@ network = MatchingNetwork(G, context_embedding_network, ways).cuda(0)
 
 optimizer = torch.optim.Adam(network.parameters())
 
-ckpt = dl.Checkpoint('results/matching/omniglot', max_to_keep=10, device=0, save_best_only=True, saving_metric='test_acc')
+ckpt = dl.Checkpoint('results/matching/omniglot1', max_to_keep=10, device=0, save_best_only=True, saving_metric='test_acc')
 acc = dl.MetricAccuracy(name='acc', device=0)
 
 
@@ -42,11 +41,15 @@ gntr = dstr.few_shot_reader(batch_size, shots+1, ways)
 test_file = f'{root}/omniglot_eval.h5'
 dste = dl.DataReader(test_file, num_workers=5, transform_func=trans)
 gnte = dste.few_shot_seq_reader(batch_size * 2, shots=shots+1, selected_classes=[0,1,2,3,4])
-listeners = [dl.Listener('test', gnte, [acc])]
+
+gnte1 = dste.few_shot_seq_reader(batch_size * 2, shots=shots+1, selected_classes=[5,6,7,8,9])
+
+listeners = [dl.Listener('test', gnte, [acc]), dl.Listener('test1', gnte1, [acc])]
+
 
 def loss_func(y_, y):
     return nn.CrossEntropyLoss()(y_.transpose(-2, -1), y)
 
 dlmodel = dl.DlModel(network, ckpt)
-dlmodel.train(gntr, loss_func, optimizer, total_steps=200000, ckpt_steps=1000, summ_steps=100, metrics=[acc], listeners=listeners, from_scratch=True)
+dlmodel.train(gntr, loss_func, optimizer, total_steps=200000, ckpt_steps=100, summ_steps=100, metrics=[acc], listeners=listeners, from_scratch=True)
 
---
+++ transformer_test.py
@@ -0,0 +1,64 @@
+import dlutil as dl
+from nlp.transformer import *
+import numpy as np
+
+
+
+
+batch_size = 64
+num_layers = 4
+d_model = 128
+dff = 512
+num_heads = 8
+drop_prob = 0.1
+
+
+def input_trans(bxs, bys):
+    bx, = bxs
+    by, = bys
+    bx = bx.astype(np.int64)
+    by = by.astype(np.int64)
+    return ((bx, by[:-1]), by[1:])
+
+file_root = '/data/examples/pt2en'
+tokenizer = torch.load(f'{file_root}/tokenizer.pt')
+enc_vocab_size = len(tokenizer[0][1]) + 2
+dec_vocab_size = len(tokenizer[1][1]) + 2
+
+dstr = dl.DataReader(f'{file_root}/pt2en_tr.h5', transform_func=input_trans, num_workers=5)
+gntr = dstr.common_reader(batch_size)
+dstv = dl.DataReader(f'{file_root}/pt2en_tv.h5', transform_func=input_trans, num_workers=5)
+gntv = dstv.common_reader(batch_size * 3, shuffle=False)
+
+
+network = Transformer(num_layers, num_heads, d_model, dff, enc_vocab_size, dec_vocab_size).cuda(0)
+
+optimizer = torch.optim.Adam(network.parameters())
+
+class CustomSchedule(torch.optim.lr_scheduler._LRScheduler):
+    def __init__(self, optimizer, d_model, warmup_steps=4000, last_epoch=-1):
+        self.d_model = d_model
+        self.warmup_steps = warmup_steps
+        super(CustomSchedule, self).__init__(optimizer, last_epoch)
+
+    def get_lr(self):
+        arg1 = 1. / (np.sqrt(self.last_epoch) + 1e-7)
+        arg2 = self.last_epoch * (self.warmup_steps ** -1.5)
+        lr = 1. / np.sqrt(self.d_model) * np.minimum(arg1, arg2)
+        return [lr for _ in self.base_lrs]
+scheduler = CustomSchedule(optimizer, d_model, warmup_steps=100)
+
+ckpt = dl.Checkpoint('results/pt2en_transformer', max_to_keep=10, save_best_only=True, saving_metric='test_acc', device=0)
+acc = dl.MetricAccuracy(name='acc', device=0)
+listeners = [dl.Listener('test', gntv, acc)]
+
+def loss_func(predictions, labels):
+    batch_size = predictions.size(0)
+    loss_ = nn.CrossEntropyLoss(reduction='none')(predictions.transpose(-1, -2), labels)
+    mask = (labels != 0).reshape((batch_size, -1)).type(loss_.dtype)
+    loss_ *= mask
+    return torch.mean(loss_)
+
+dlmodel = dl.DlModel(network, ckpt)
+dlmodel.train(gntr, loss_func, optimizer, num_epochs=100, metrics=[acc], listeners=listeners, from_scratch=True, scheduler=scheduler)
+
