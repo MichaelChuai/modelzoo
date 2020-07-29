edc774aaf66ea53f0f27db58034a958093c2fc68
MichaelChuai <18alexanderm117@tongji.edu.cn>
2020-07-29 11:16:43 +0800

    m

--- evo/t1.py
+++ evo/t1.py
@@ -10,11 +10,32 @@ class Model:
     
     def __str__(self):
         return f'{self.arch:b}'
-    
-
 
     @classmethod
     def sum_bits(cls, arch):
-        total = 0
-        for _ in range(dim):
-            total += arch
+        total = sum(map(int, bin(arch)[2:]))
+        return total
+
+    @classmethod
+    def random_arch(cls):
+        return random.randint(0, 2 ** dim - 1)
+
+    @classmethod
+    def train_and_eval(cls, arch):
+        accuracy = cls.sum_bits(arch) / dim
+        accuracy += random.gauss(0.0, noise_stdev)
+        accuracy = 0.0 if accuracy < 0.0 else accuracy
+        accuracy = 1.0 if accuracy > 1.0 else accuracy
+        return accuracy
+
+    @classmethod
+    def mutate_arch(cls, prev_arch):
+        position = random.randint(0, dim-1)
+        arch = prev_arch ^ (1 << position)
+        return arch
+
+
+q = Model.random_arch()
+acc = Model.train_and_eval(q)
+q2 = Model.mutate_arch(q)
+acc2 = Model.train_and_eval(q2)
